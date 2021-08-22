bd0c66f6cff90f9396114ffb2d74493d3a247fe7
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-08-21 23:18:23 -0300

    Updated repo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import TextField, { Input } from '@material/react-text-field';
 
-import { Container } from './styles';
+import { Container, Search } from './styles';
+
+import logo from '../../assets/logo.svg';
 
 const Home = () => {
-  return <Container>Hello world;</Container>;
+  return (
+    <Container>
+      <Search>
+        <img src={logo} alt="Logo do restaurante" />
+        <TextField label="Pesquisar" onTrailingIconSelect={() => this.setState({ value: '' })}>
+          <Input
+            value={this.state.value}
+            onChange={(e) => this.setState({ value: e.currentTarget.value })}
+          />
+        </TextField>
+      </Search>
+    </Container>
+  );
 };
 
 export { Home };
